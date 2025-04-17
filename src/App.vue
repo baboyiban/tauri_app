36893e4a691bfa3c1ec8da63fcfd337e03803d5b
baboyiban <baboyiban@gmail.com>
2025-04-17 15:36:29 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+const a = ref(0);
+const b = ref(0);
+const result = ref("");
+
+async function calc() {
+    result.value = await invoke("calc", { a: a.value, b: b.value });
+}
+</script>
+
+<template>
+    <main>
+        <input v-model="a" type="string" />
+        <input v-model="b" type="string" />
+        <button @click="calc">calc</button>
+        <div>{{ result }}</div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    background-color: #f0f0f0;
+}
+</style>
+<style></style>
